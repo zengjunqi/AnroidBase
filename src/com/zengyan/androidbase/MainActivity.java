4be59de8f0c685d5a802965daf422ad40a536f51
zengjunqi <zengjunqi@github.com>
2014-11-14 10:17:47 +0800

    增加杀死进程,杀死自己进程与杀死其他程序进程

--- src/com/zengyan/androidbase/MainActivity.java
+++ src/com/zengyan/androidbase/MainActivity.java
@@ -1,6 +1,10 @@
 package com.zengyan.androidbase;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,13 +18,16 @@ public class MainActivity extends Activity {
 	private ListView lv;
 	private String[] items = { "CallPhoneAndSendMsg", "ReaderAndWriterFile",
 			"XmlParserAct", "GetSDInfo", "Sqlite", "PhoneListener",
-			"ImageProcess", "SiYiFu" ,"MediaPlayer Play Muisc","MediaPlayer Play Network Music","MediaPlayer Play Video",
-			"Dialog","Notification","Animation","Sensor"};
+			"ImageProcess", "SiYiFu", "MediaPlayer Play Muisc",
+			"MediaPlayer Play Network Music", "MediaPlayer Play Video",
+			"Dialog", "Notification", "Animation", "Sensor","KillProcess" };
 	private Class[] cc = { CallPhoneAndSendMsg.class,
 			ReaderAndWriterFile.class, XmlParserAct.class, GetSDInfo.class,
-			Sqlite.class, PhoneListener.class, ImageProcess.class, SiYiFu.class,
-			MediaPlayerPlaySDMusic.class,MediaPlayerPlayNetworkMusic.class,MediaPlayerPlayVideo.class,
-			DialogFactory.class,Nitification.class,MyAnimation.class,MySensor.class};
+			Sqlite.class, PhoneListener.class, ImageProcess.class,
+			SiYiFu.class, MediaPlayerPlaySDMusic.class,
+			MediaPlayerPlayNetworkMusic.class, MediaPlayerPlayVideo.class,
+			DialogFactory.class, Nitification.class, MyAnimation.class,
+			MySensor.class,MyKillProcess.class };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +60,26 @@ public class MainActivity extends Activity {
 		ArrayAdapter adapter = new ArrayAdapter(this,
 				android.R.layout.simple_list_item_1, items);
 		lv.setAdapter(adapter);
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		// super.onBackPressed();
+
+		AlertDialog.Builder builder = new Builder(this);
+		builder.setTitle("确定退出程序");
+		builder.setMessage("你确定要退出程序吗?");
+		builder.setNegativeButton("取消", null);
+		builder.setPositiveButton("确定", new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				android.os.Process.killProcess(android.os.Process.myPid());
+			}
+		});
+		builder.create().show();
 	}
 
 }
---
+++ src/com/zengyan/androidbase/MyKillProcess.java
@@ -0,0 +1,36 @@
+package com.zengyan.androidbase;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MyKillProcess extends Activity {
+	private Button btnKill;
+	private EditText etpackage;
+	private ActivityManager am;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.killprocess_activity);
+		btnKill=(Button) findViewById(R.id.btnKill);
+		etpackage=(EditText) findViewById(R.id.txtPackage);
+		am=(ActivityManager) getSystemService(ACTIVITY_SERVICE);
+		btnKill.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				am.killBackgroundProcesses(etpackage.getText().toString());//能杀死空进程和后台进程
+				Toast.makeText(MyKillProcess.this, "杀死成功", 1000).show();
+			}
+		});
+
+	}
+}
